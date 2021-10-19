b28247ef9d36457a6408b24f559f6039189100a1
CrappyDill <mddillon47@yahoo.com>
2021-10-19 09:14:50 -0700

    morgan middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const ExpressError = require("./expressError");
 const itemsRoutes = require("./itemsRoutes");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/items", itemsRoutes);
 
--- itemsRoutes.js
+++ itemsRoutes.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = new express.Router();
+const ExpressError = require("./expressError");
+const items = require("./fakeDb")
 
-const ITEMS = [{ name: "butter", price: 1.99 }];
+const items = [{ name: "butter", price: 1.99 }];
 
 router.get("/", (req, res, next) => {
-  res.json({ items: ITEMS });
+  res.json({ items });
 });
 
+router.post("/", (req, res, next) => {
+  const newItem = { name: req.body.name, price: req.body.price }
+  items.push(newItem)
+  res.status(201).json({item: newItem})
+})
+
 router.get("/:name", (req, res, next) => {
-  const item = ITEMS.find((i) => i.name === req.params.name);
+  const item = items.find((i) => i.name === req.params.name);
+  if (item === undefined) {
+    throw new ExpressError("Item not found", 404)
+  }
   res.json({ item });
 });
+
+router.patch("/:name")
 
 module.exports = router;
